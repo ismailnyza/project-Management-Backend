9b142fd50ace76adf66f65885e9cab2431bb815c
ismailN <ismailnyzarsl@gmail.com>
2026-01-08 09:26:58 +0530

    Dusting Some cobwebs

--- src/main/java/com/projectManagementAPI/model/Main.java
+++ src/main/java/com/projectManagementAPI/model/Main.java
@@ -3,17 +3,21 @@ package com.projectManagementAPI.model;
 import com.projectManagementAPI.model.controller.BoardController;
 import com.projectManagementAPI.model.repository.BoardRepository;
 import com.projectManagementAPI.model.service.BoardService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static spark.Spark.port;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         port(8000);
 
         BoardRepository boardRepository = new BoardRepository();
         BoardService boardService = new BoardService(boardRepository);
         new BoardController(boardService);
-        System.out.println("server is up");
+        log.info("main started");
 
     }
 }
